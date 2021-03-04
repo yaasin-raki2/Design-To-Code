14df635f8bda72bf43829d9c5d2407ae499d5b2e
yaasin-raki2 <yassin23raki@gmail.com>
2021-03-04 19:32:21 +0100

    installing jest and supertest with their TD, Setting up a testing environement

---
+++ Api/src/test/setup.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+let mongo: any;
+
+beforeAll(async () => {
+  await mongoose.connect("mongodb://localhost/test", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
