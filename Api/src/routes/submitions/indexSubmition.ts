a0629f2b0ed7878b499ebefb6e547261a8f79a55
yaasin-raki2 <yassin23raki@gmail.com>
2021-03-21 11:32:06 +0100

    Refactoring the Index Submitions Route Handler

--- Api/src/routes/submitions/indexSubmition.ts
+++ Api/src/routes/submitions/indexSubmition.ts
@@ -1,11 +1,23 @@
 import express, { Request, Response } from "express";
+import { NotFoundError } from "../../errors/not-found-error";
+import { requireAuth } from "../../middlewares/require-auth";
 
 import { Submition } from "../../models/submition";
 
 const router = express.Router();
 
-router.get("/api/submitions", async (req: Request, res: Response) => {
-  const submitions = await Submition.find({});
+router.get("/api/submitions", requireAuth, async (req: Request, res: Response) => {
+  let submitions: any;
+
+  if (req.body.designName === undefined) {
+    submitions = await Submition.find({ userId: req.currentUser!.id });
+
+    if (!submitions) throw new NotFoundError();
+  } else {
+    submitions = await Submition.find({ designName: req.body.designName });
+
+    if (!submitions) throw new NotFoundError();
+  }
 
   res.status(200).send(submitions);
 });
