6253d98c0e52f4da366f9e9147ecb8f79ccbccc5
yaasin-raki2 <yassin23raki@gmail.com>
2021-04-24 22:51:32 +0000

    Creating a user reducer inside redux

---
+++ client/src/redux/user/user.reducer.ts
@@ -0,0 +1,40 @@
+import { UserActionTypes } from "./user.types.d";
+import { UserPayloadTD } from "./user.payload.d";
+import { Action } from "./user.actions.d";
+
+interface UserState {
+  currentUser: UserPayloadTD | null;
+  errors: any;
+}
+
+const initialState = {
+  currentUser: null,
+  errors: null,
+};
+
+const userReducer = (state: UserState = initialState, action: Action) => {
+  switch (action.type) {
+    case UserActionTypes.SIGN_UP_SUCCESS:
+      return {
+        ...state,
+        currentUser: action.payload,
+        error: null,
+      };
+    case UserActionTypes.LOG_IN_SUCCESS:
+      return {
+        ...state,
+        currentUser: null,
+        error: null,
+      };
+    case UserActionTypes.SIGN_UP_FAILURE:
+    case UserActionTypes.LOG_IN_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
