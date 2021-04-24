54f994d7fbfd0c832aecaefb53ee6c6635aa99a0
yaasin-raki2 <yassin23raki@gmail.com>
2021-04-24 22:08:21 +0000

    Enhancing the user redux type definitions

--- client/src/redux/user/user.actions.d.ts
+++ client/src/redux/user/user.actions.d.ts
@@ -1,12 +1,40 @@
 import { UserActionTypes } from "./user.types";
 import { UserPayloadTD } from "./user.payload";
 
-export interface SignUp {
-  type: UserActionTypes.SIGN_UP;
+export interface SignUpStartAction {
+  type: UserActionTypes.SIGN_UP_START;
+  payload: {
+    userName: string;
+    email: string;
+    password: string;
+    userType: string;
+  };
+}
+
+export interface SignUpFailureAction {
+  type: UserActionTypes.SIGN_UP_FAILURE;
+  payload: any;
+}
+
+export interface SignUpSuccessAction {
+  type: UserActionTypes.SIGN_UP_SUCCESS;
   payload: UserPayloadTD;
 }
 
-export interface LogIn {
-  type: UserActionTypes.LOG_IN;
+export interface LogInStartAction {
+  type: UserActionTypes.LOG_IN_START;
+  payload: {
+    email: string;
+    password: string;
+  };
+}
+
+export interface LogInFailureAction {
+  type: UserActionTypes.LOG_IN_FAILURE;
+  payload: any;
+}
+
+export interface LogInSuccessAction {
+  type: UserActionTypes.LOG_IN_SUCCESS;
   payload: UserPayloadTD;
 }
--- client/src/redux/user/user.types.d.ts
+++ client/src/redux/user/user.types.d.ts
@@ -1,4 +1,8 @@
 export enum UserActionTypes {
-  SIGN_UP = "sign-up",
-  LOG_IN = "log-in",
+  SIGN_UP_START = "sign-up-start",
+  SIGN_UP_FAILURE = "sign-up-failure",
+  SIGN_UP_SUCCESS = "sign-up-success",
+  LOG_IN_START = "log-in-start",
+  LOG_IN_FAILURE = "log-in-failure",
+  LOG_IN_SUCCESS = "log-in-success",
 }
