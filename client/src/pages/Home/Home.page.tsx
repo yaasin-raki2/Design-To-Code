738161fe417463cc9f39c3235abbbffbb65dabde
yaasin-raki2 <yassin23raki@gmail.com>
2021-05-01 20:30:55 +0000

    Home Page for Iphone 11 pro max done

--- client/src/pages/Home/Home.page.tsx
+++ client/src/pages/Home/Home.page.tsx
@@ -3,9 +3,8 @@ import { FC } from "react";
 import {
   HomePageWrapper,
   HeroTitle,
-  HeroTextWrapper,
   HeroBody,
-  Button,
+  HeroButton,
   HeroIllustration,
   HeroSectionWrapper,
 } from "./Home.styles";
@@ -15,16 +14,14 @@ const HomePage: FC = () => {
   return (
     <HomePageWrapper>
       <HeroSectionWrapper>
-        <HeroTextWrapper>
-          <HeroTitle>We Help You Build Your Next Outstanding Portfolio Project</HeroTitle>
-          <HeroBody>
-            Build large scale Web / Mobile applications with your favorite technology
-            using our own Recources and Case Studies and Design Guides, to sharppen your
-            skills and to get an outstanding portfolio project that will eventually
-            separate you from the crowd
-          </HeroBody>
-          <Button to="/">Browse Designs</Button>
-        </HeroTextWrapper>
+        <HeroTitle>We Help You Build Your Next Outstanding Portfolio Project</HeroTitle>
+        <HeroBody>
+          Build large scale Web / Mobile applications with your favorite technology using
+          our own Recources and Case Studies and Design Guides, to sharppen your skills
+          and to get an outstanding portfolio project that will eventually separate you
+          from the crowd
+        </HeroBody>
+        <HeroButton to="/">Browse Designs</HeroButton>
         <HeroIllustration src={heroIllustration} />
       </HeroSectionWrapper>
     </HomePageWrapper>
--- client/src/pages/Home/Home.styles.ts
+++ client/src/pages/Home/Home.styles.ts
@@ -3,16 +3,35 @@ import { Link } from "react-router-dom";
 import SVG from "react-inlinesvg";
 
 export const HomePageWrapper = styled.div`
-  width: 1229px;
-  height: 521px;
+  width: 1233px;
   margin: 50px auto 0 auto;
+
+  @media (max-width: 600px) {
+    width: 364px;
+    margin: 20px auto 0 auto;
+  }
 `;
 
 export const HeroSectionWrapper = styled.div`
-  display: flex;
+  display: grid;
+  grid-template-areas:
+    "hero-title hero-illustration"
+    "hero-body hero-illustration"
+    "hero-button hero-illustration";
+  height: 575px;
+
+  @media (max-width: 600px) {
+    grid-template-areas:
+      "hero-title"
+      "hero-illustration"
+      "hero-body "
+      "hero-button";
+    height: 657px;
+  }
 `;
 
 export const HeroTitle = styled.h1`
+  grid-area: hero-title;
   margin: 0;
   font-weight: 600;
   font-size: 60px;
@@ -20,9 +39,19 @@ export const HeroTitle = styled.h1`
   width: 724px;
   height: 270px;
   color: ${({ theme }) => theme.text};
+
+  @media (max-width: 600px) {
+    width: 364px;
+    height: 144px;
+    font-size: 30px;
+    text-align: center;
+    line-height: 45px;
+    margin: auto;
+  }
 `;
 
 export const HeroBody = styled.p`
+  grid-area: hero-body;
   width: 527px;
   height: 135px;
   position: relative;
@@ -30,7 +59,7 @@ export const HeroBody = styled.p`
   font-weight: normal;
   font-size: 18px;
   line-height: 27px;
-  margin-top: 28px;
+  margin: 0;
 
   color: ${({ theme }) => theme.text};
 
@@ -43,11 +72,23 @@ export const HeroBody = styled.p`
     top: -17px;
     background-color: ${({ theme }) => theme.primary};
   }
+
+  @media (max-width: 600px) {
+    width: 364px;
+    height: 144px;
+    text-align: center;
+    font-size: 16px;
+    line-height: 24px;
+    margin: auto;
+
+    ::before {
+      content: none;
+    }
+  }
 `;
 
-export const HeroTextWrapper = styled.div``;
-
-export const Button = styled(Link)`
+export const HeroButton = styled(Link)`
+  grid-area: hero-button;
   background-color: ${({ theme }) => theme.primary};
   width: 279px;
   height: 60px;
@@ -61,7 +102,6 @@ export const Button = styled(Link)`
   align-items: center;
   justify-content: center;
   font-weight: 500;
-  margin-top: 44px;
 
   :hover {
     filter: brightness(120%);
@@ -70,8 +110,19 @@ export const Button = styled(Link)`
   :active {
     transform: scale(0.5);
   }
+
+  @media (max-width: 600px) {
+    margin: auto;
+  }
 `;
 
 export const HeroIllustration = styled(SVG)`
+  grid-area: hero-illustration;
   margin-top: 18px;
+
+  @media (max-width: 600px) {
+    width: 202px;
+    height: 221px;
+    margin: auto;
+  }
 `;
